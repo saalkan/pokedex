e2593d96c250a68928d6efe79f650b95a5166679
saalkan <s.asligulalkan@gmail.com>
2024-06-17 20:22:12 +0300

    Pokemon api - desktop

---
+++ index.js
@@ -0,0 +1,21 @@
+// source: https://youtu.be/37vxWr0WgQk
+
+//fetch_pokemon();
+
+async function fetch_pokemon() {
+  try {
+    const pokemon_id = Math.floor((Math.random() * 1025) + 1);
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon_id}`);
+    if (!response.ok) {
+      throw new Error(`Invalid pokemon.`);
+    }
+    const data = await response.json();
+    const pokemon_image = data.sprites.front_default;
+    const image_element = document.getElementById(`pokemon-image`);
+    image_element.src = pokemon_image;
+    image_element.style.display = `block`;
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
