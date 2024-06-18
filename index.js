aab445ed02ab231980cefc10adaf9c5006ee0858
saalkan <s.asligulalkan@gmail.com>
2024-06-18 19:01:26 +0300

    Screen - pokemon name

--- index.js
+++ index.js
@@ -9,11 +9,18 @@ async function fetch_pokemon() {
     if (!response.ok) {
       throw new Error(`Invalid pokemon.`);
     }
+
     const data = await response.json();
+
     const pokemon_image = data.sprites.front_default;
     const image_element = document.getElementById(`pokemon-image`);
     image_element.src = pokemon_image;
     image_element.style.display = `block`;
+
+    const pokemon_name = data.species.name;
+    const name_element = document.getElementById(`pokemon-name`);
+    name_element.innerText = pokemon_name;
+    name_element.style.display = "inline";
   }
   catch (error) {
     console.error(error);
